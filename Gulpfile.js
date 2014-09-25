36af8993d5769cd0ba1b92cd8361ebe1f2303af3
Juan Cahuana <jcahuana@vertice.pe>
2014-09-25 18:02:18 -0500

    Cambiamos la forma de optimizar el html, sólo usando jade

--- Gulpfile.js
+++ Gulpfile.js
@@ -94,13 +94,13 @@ gulp.task('photos', function(){
 
 gulp.task('jade', function(){
 	gulp.src(['./app/jade/*.jade', '!./app/jade/portafolio-interior.jade'])
-		.pipe(jade({ pretty: true }))
+		.pipe(jade({ pretty: false }))
 		.pipe(gulp.dest('./app'));
 });
 
 gulp.task('html', function(){
 	gulp.src('./app/*.html')
-		.pipe(minifyHTML())
+		//.pipe(minifyHTML())
 		.pipe(gulp.dest('./public'));
 });
 
